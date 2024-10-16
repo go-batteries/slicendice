c80b238550b4307e8b582403af90ef00e77301db
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2024-10-16 14:45:10 +0530

    feat: testing the index returned from tranform in func iterator working properly

--- lazy/hofs.go
+++ lazy/hofs.go
@@ -3,54 +3,48 @@ package lazy
 import "github.com/go-batteries/slicendice"
 
 func Map[E, V any](elements []E, mapper slicendice.MapperFunc[E, V]) Iter[V] {
-	var zero V
-
 	iter := ToSliceIter(elements)
 
-	return NewFuncIterator(func() (bool, V) {
-		ok, val := iter.Next()
-		if !ok {
-			return false, zero
-		}
-
-		result := mapper(val, iter.index)
-		return true, result
-	})
-}
-
-func Filter[E any](elements []E, filterer slicendice.FilterFunc[E]) Iter[E] {
-	var zero E
-
-	iter := ToSliceIter(elements)
-
-	return NewFuncIterator(func() (bool, E) {
-		for {
-			ok, val := iter.Next()
-			if !ok {
-				return false, zero
-			}
-
-			if filterer(val, iter.index) {
-				return true, val
-			}
-		}
-	})
-}
-
-func Map2[E, V any](elements []E, mapper slicendice.MapperFunc[E, V]) Iter[V] {
-	iter := ToSliceIter(elements)
-
-	return NewFuncIterator2(iter, func(e E, _ int) (bool, V) {
+	return NewFuncIterator(iter, func(e E, _ int) (bool, V) {
 		return true, mapper(e, iter.index)
 	})
 
 }
 
-func Filter2[E any](elements []E, mapper slicendice.FilterFunc[E]) Iter[E] {
+func MapFromIter[E, V any](iter Iter[E], mapper slicendice.MapperFunc[E, V]) Iter[V] {
+	return NewFuncIterator(iter, func(e E, i int) (bool, V) {
+		return true, mapper(e, i)
+	})
+
+}
+
+func Filter[E any](elements []E, mapper slicendice.FilterFunc[E]) Iter[E] {
 	iter := ToSliceIter(elements)
 
-	return NewFuncIterator2(iter, func(e E, _ int) (bool, E) {
+	return NewFuncIterator(iter, func(e E, _ int) (bool, E) {
 		return mapper(e, iter.index), e
 	})
 
+}
+
+func FilterFromIter[E any](iter Iter[E], mapper slicendice.FilterFunc[E]) Iter[E] {
+	return NewFuncIterator(iter, func(e E, i int) (bool, E) {
+		return mapper(e, i), e
+	})
+
+}
+
+func Take[E any](elements []E, n int) Iter[E] {
+	var zero E
+
+	iter := ToSliceIter(elements)
+
+	return NewFuncIterator(iter, func(e E, _ int) (bool, E) {
+		if n > 0 {
+			n -= 1
+			return true, e
+		}
+
+		return false, zero
+	})
 }
--- lazy/hofs_test.go
+++ lazy/hofs_test.go
@@ -2,6 +2,7 @@ package lazy_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/go-batteries/slicendice/lazy"
@@ -27,17 +28,6 @@ func Test_MapIter(t *testing.T) {
 	t.Run("double item in array", func(t *testing.T) {
 		input := []int{1, 3, 4}
 		mapper := lazy.Map(input, adder)
-		counter := 0
-
-		for ok, res := mapper.Next(); ok; ok, res = mapper.Next() {
-			assert.Equal(t, adder(input[counter], 0), res)
-			counter += 1
-		}
-	})
-
-	t.Run("double item in array", func(t *testing.T) {
-		input := []int{1, 3, 4}
-		mapper := lazy.Map2(input, adder)
 		counter := 0
 
 		for ok, res := mapper.Next(); ok; ok, res = mapper.Next() {
@@ -75,21 +65,88 @@ func Test_FilterIter(t *testing.T) {
 
 		assert.Equal(t, []string{"a", "a"}, res)
 	})
+}
 
-	t.Run("filter items one by one by one", func(t *testing.T) {
-		input := []string{"a", "b", "a", "c"}
-		afilters := func(el string, _ int) bool {
-			return el == "a"
-		}
+func Test_Take(t *testing.T) {
+	t.Run("empty array takes none", func(t *testing.T) {
+		input := []int{}
 
-		filterer := lazy.Filter2(input, afilters)
-		res := []string{}
+		iter := lazy.Take(input, 5)
 
-		for ok, out := filterer.Next(); ok; ok, out = filterer.Next() {
-			res = append(res, out)
-		}
+		ok, next := iter.Next()
 
-		assert.Equal(t, []string{"a", "a"}, res)
+		assert.Equal(t, false, ok)
+		assert.Empty(t, next)
 	})
 
+	t.Run("return all items when len(elements) < takeN", func(t *testing.T) {
+		input := []int{1, 2}
+		iter := lazy.Take(input, 5)
+
+		result := []int{}
+
+		for ok, e := iter.Next(); ok; ok, e = iter.Next() {
+			result = append(result, e)
+		}
+
+		assert.Equal(t, len(input), len(result))
+		assert.Equal(t, input, result)
+	})
+
+	t.Run("returns only takeN elements", func(t *testing.T) {
+		input := []int{1, 2, 3, 4, 5, 6}
+		iter := lazy.Take(input, 3)
+
+		result := []int{}
+
+		for ok, e := iter.Next(); ok; ok, e = iter.Next() {
+			result = append(result, e)
+		}
+
+		assert.Equal(t, 3, len(result))
+		assert.Equal(t, []int{1, 2, 3}, result)
+
+	})
+}
+
+func Test_MapFromIter(t *testing.T) {
+	inputs := []int{1, 2, 3, 4}
+
+	inputIter := lazy.ToSliceIter(inputs)
+	adder := func(el int, i int) string {
+		return fmt.Sprintf("%d:%d", el, i)
+	}
+
+	iter := lazy.MapFromIter(inputIter, adder)
+
+	count := 0
+
+	for ok, res := iter.Next(); ok; ok, res = iter.Next() {
+		splits := strings.Split(res, ":")
+
+		assert.Equal(t, fmt.Sprintf("%d", inputs[count]), splits[0])
+		assert.Equal(t, fmt.Sprintf("%d", count), splits[1])
+
+		count += 1
+	}
+}
+
+func Test_FilterFromIter(t *testing.T) {
+	inputs := []int{1, 2, 3, 4}
+	idxMap := map[int]int{1: 0, 2: 1, 3: 2, 4: 3}
+
+	inputIter := lazy.ToSliceIter(inputs)
+	evens := func(el int, i int) bool {
+		assert.Equal(t, idxMap[el], i)
+		return el%2 == 0
+	}
+
+	iter := lazy.FilterFromIter(inputIter, evens)
+	result := []int{}
+
+	for ok, next := iter.Next(); ok; ok, next = iter.Next() {
+		result = append(result, next)
+	}
+
+	assert.Equal(t, []int{2, 4}, result)
 }
